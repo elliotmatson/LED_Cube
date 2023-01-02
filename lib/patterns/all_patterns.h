f465eee5a6d898e4eb35a976964578144428fafa
Elliot Matson <ematson5897@gmail.com>
2023-01-02 10:59:37 -0600

    Move into folders

--- lib/patterns/all_patterns.h
+++ lib/patterns/all_patterns.h
@@ -2,8 +2,9 @@
 #define ALL_PATTERNS_H
 
 // Convenience include file. All patterns get included here
-#include "plasma.h"
-#include "snakes.h"
+#include "snakes/snakes.h"
+#include "plasma/plasma.h"
+#include "spotify/spotify.h"
 
 // Enum for all patterns
 enum patterns

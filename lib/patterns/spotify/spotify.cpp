3cecbbd705bf27d29bdfbc92c0c559f070a693e1
Elliot Matson <ematson5897@gmail.com>
2023-06-17 16:45:36 -0500

    Spotify refactor

--- lib/patterns/spotify/spotify.cpp
+++ lib/patterns/spotify/spotify.cpp
@@ -29,6 +29,36 @@ Spotify::~Spotify()
     stop();
 }
 
+void Spotify::changeStatus(PatternStatus status)
+{
+    if (status != patternStatus)
+    {
+        ESP_LOGI(__func__, "Changing status to %d", status);
+        patternStatus = status;
+        switch (status)
+        {
+            case oauth:
+                this->pattern->display->fillScreen(0x0000);
+                panel0->setCursor(0, 0);
+                panel0->print("No OAuth\nCredentials");
+                break;
+            case refreshToken:
+                this->pattern->display->fillScreen(0x0000);
+                panel0->setCursor(0, 0);
+                panel0->print("Not logged\nin...\n\ncube.local\n/spotify");
+                break;
+            case noPlayback:
+                vTaskSuspend(progressTask);
+                this->pattern->display->fillScreen(0x0000);
+                break;
+            case playback:
+                this->pattern->display->fillScreen(0x0000);
+                vTaskResume(progressTask);
+                break;
+        }
+    }
+}
+
 void Spotify::init(PatternServices *pattern)
 {
     this->pattern = pattern;
@@ -36,33 +66,102 @@ void Spotify::init(PatternServices *pattern)
     panel1 = new SinglePanel(*pattern->display, 1, 2);
     panel2 = new SinglePanel(*pattern->display, 0, 0);
     spotify = new SpotifyArduino(this->client);
-  spotifyPrefs.begin("spotify");
+    spotifyPrefs.begin("spotify");
+    TJpgDec.setJpgScale(1);
+    TJpgDec.setCallback([&](int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_t *bitmap)
+                        { return this->displayImageOutput(x, y, w, h, bitmap); });
 
+    changeStatus(oauth);
+}
+
+void Spotify::start()
+{
+    xTaskCreate(
+        [](void *o)
+        { static_cast<Spotify *>(o)->refreshInfo(); }, // This is disgusting, but it works
+        "Spotify - Refresh",                    // Name of the task (for debugging)
+        10000,                                  // Stack size (bytes)
+        this,                                   // Parameter to pass
+        1,                                      // Task priority
+        &refreshTask                            // Task handle
+    );
+
+    xTaskCreate(
+        [](void *o)
+        { static_cast<Spotify *>(o)->displayProgress(); }, // This is disgusting, but it works
+        "Spotify - Display Progress",                      // Name of the task (for debugging)
+        2000,                                              // Stack size (bytes)
+        this,                                              // Parameter to pass
+        1,                                                 // Task priority
+        &progressTask                                      // Task handle
+    ); 
+    vTaskSuspend(progressTask);
+
+    setupCredentials();
+}
+
+int Spotify::setupCredentials()
+{
+    // Get Spotify Credentials
 #ifdef SPOTIFY_CLIENT_ID
-  spotifyPrefs.putString("SPOTIFY_ID", SPOTIFY_CLIENT_ID);
-  ESP_LOGI(__func__, "SPOTIFY_CLIENT_ID: %s", SPOTIFY_CLIENT_ID);
+    spotifyPrefs.putString("SPOTIFY_ID", SPOTIFY_CLIENT_ID);
 #endif
 #ifdef SPOTIFY_CLIENT_SECRET
-  spotifyPrefs.putString("SPOTIFY_SECRET", SPOTIFY_CLIENT_SECRET);
-  ESP_LOGI(__func__, "SPOTIFY_CLIENT_SECRET: %s", SPOTIFY_CLIENT_SECRET);
+    spotifyPrefs.putString("SPOTIFY_SECRET", SPOTIFY_CLIENT_SECRET);
 #endif
+    // Reset credentials if needed
+#ifdef SPOTIFY_RESET_OAUTH
+    spotifyPrefs.remove("SPOTIFY_ID");
+    spotifyPrefs.remove("SPOTIFY_SECRET");
+#endif
+#ifdef SPOTIFY_RESET_TOKEN
+    spotifyPrefs.remove("SPOTIFY_TOKEN");
+#endif
+    spotifyPrefs.getString("SPOTIFY_ID", "").toCharArray(spotifyID, 33);
+    spotifyPrefs.getString("SPOTIFY_SECRET", "").toCharArray(spotifySecret, 33);
 
-  spotifyPrefs.getString("SPOTIFY_ID").toCharArray(spotifyID, 33);
-  spotifyPrefs.getString("SPOTIFY_SECRET").toCharArray(spotifySecret, 33);
+    if (spotifyID[0] == '\0' || spotifySecret[0] == '\0')
+    {
+        ESP_LOGE(__func__, "Spotify ID or Secret not set");
+        return -2;
+    }
 
-  ESP_LOGI(__func__, "Setting up Spotify Library");
-  client.setCACertBundle(rootca_crt_bundle_start);
-  spotify->lateInit(spotifyID, spotifySecret, spotifyPrefs.getString("SPOTIFY_TOKEN").c_str());
+    changeStatus(refreshToken);
 
-  ESP_LOGI(__func__, "Setting up callbacks");
-  pattern->server->on("/spotify", HTTP_GET, [&](AsyncWebServerRequest *request)
-                      {    
+    // Initialize Spotify Library
+    ESP_LOGI(__func__, "Setting up Spotify Library");
+    client.setCACertBundle(rootca_crt_bundle_start);
+    spotify->lateInit(spotifyID, spotifySecret);
+
+    if (spotifyPrefs.getString("SPOTIFY_TOKEN", "").equals(""))
+    {
+        startOauthWebServer();
+        ESP_LOGE(__func__, "No token found");
+        return -1;
+    } else {
+        ESP_LOGI(__func__, "Token found");
+        spotify->setRefreshToken(spotifyPrefs.getString("SPOTIFY_TOKEN").c_str());
+        changeStatus(noPlayback);
+    }
+    ESP_LOGI(__func__, "Refreshing Access Tokens");
+    if (!spotify->refreshAccessToken())
+    {
+        ESP_LOGI(__func__, "Failed to get access tokens");
+    }
+    return 1;
+}
+
+void Spotify::startOauthWebServer()
+{
+    ESP_LOGI(__func__, "Not logged in, Setting up API handlers");
+    handlers.push_back(&pattern->server->on("/spotify", HTTP_GET, [&](AsyncWebServerRequest *request)
+                                      {    
                     char webpage[800];
                     sprintf(webpage, webpageTemplate, spotifyPrefs.getString("SPOTIFY_ID").c_str(), callbackURI, scope);
                     request->send(200, "text/html", webpage);
-                    ESP_LOGI(__func__,"got root request"); });
-  pattern->server->on("/callback/", HTTP_GET, [&](AsyncWebServerRequest *request)
-                      {    
+                    ESP_LOGI(__func__,"got root request"); }));
+    handlers.push_back(&pattern->server->on("/callback/", HTTP_GET, [&](AsyncWebServerRequest *request)
+                        {    
                     String code = "";
                     const char *refreshToken = NULL;
                     ESP_LOGI(__func__,"got callback request");
@@ -82,72 +181,32 @@ void Spotify::init(PatternServices *pattern)
                         spotifyPrefs.putString("SPOTIFY_TOKEN", refreshToken);
                         request->send(200, "text/plain", refreshToken);
                         ESP_LOGI(__func__,"got token: %s", refreshToken);
+                        spotify->setRefreshToken(refreshToken);
+                        this->changeStatus(noPlayback);
+                        stopOauthWebServer();
                     }
                     else
                     {
                         request->send(404, "text/plain", "Failed to load token, check serial monitor");
                         ESP_LOGI(__func__,"Failed to load token, check serial monitor");
-                    } });
-  pattern->server->onNotFound([&](AsyncWebServerRequest *request)
-                              {
-                        String message = "File Not Found\n\n";
-                        message += "URI: ";
-                        message += request->url();
-                        message += "\nMethod: ";
-                        message += (request->method() == HTTP_GET) ? "GET" : "POST";
-                        message += "\nArguments: ";
-                        message += request->args();
-                        message += "\n";
-
-                        for (uint8_t i = 0; i < request->args(); i++)
-                        {
-                            message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
-                        }
-
-                        request->send(404, "text/plain", message);
-
-                        ESP_LOGI(__func__,"%s", message.c_str()); });
-  ESP_LOGI(__func__, "HTTP server started");
-  TJpgDec.setJpgScale(1);
-
-  // The decoder must be given the exact name of the rendering function above
-  TJpgDec.setCallback([&](int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_t *bitmap)
-                      { return this->displayOutput(x, y, w, h, bitmap); });
-
-  ESP_LOGI(__func__, "Refreshing Access Tokens");
-  if (!spotify->refreshAccessToken())
-  {
-      ESP_LOGI(__func__, "Failed to get access tokens");
-  }
+                    } }));
+    ESP_LOGI(__func__, "HTTP server started");
+    ESP_LOGI(__func__, "No token found, please visit http://cube.local/spotify to authenticate");
 }
 
-void Spotify::start()
-{
-    this->pattern->display->fillScreen(0x0000);
-    if (spotifyPrefs.getString("SPOTIFY_TOKEN").c_str()) {
-        xTaskCreate(
-            [](void *o)
-            { static_cast<Spotify *>(o)->show(); }, // This is disgusting, but it works
-            "Spotify - Refresh",                    // Name of the task (for debugging)
-            10000,                                 // Stack size (bytes)
-            this,                                 // Parameter to pass
-            1,                                    // Task priority
-            &refreshTask                          // Task handle
-        );
-
-        xTaskCreate(
-            [](void *o)
-            { static_cast<Spotify *>(o)->displayProgress(); }, // This is disgusting, but it works
-            "Spotify - Display Progress",                      // Name of the task (for debugging)
-            2000,                                              // Stack size (bytes)
-            this,                                              // Parameter to pass
-            1,                                                 // Task priority
-            &progressTask                                      // Task handle
-        );
-    } else {
-        ESP_LOGI(__func__, "No token found, please visit http://cube.local/spotify to authenticate");
-    }
+void Spotify::stopOauthWebServer()
+{   
+    ESP_LOGI(__func__, "Removing HTTP server handlers");
+    // iterate through handlers vector and remove all handlers
+    for (uint8_t i = 0; i < handlers.size(); i++)
+    {
+        pattern->server->removeHandler(handlers[i]);
+    } 
 }
+
+
+
+
 
 void Spotify::stop()
 {
@@ -159,49 +218,92 @@ void Spotify::stop()
     vTaskDelete(progressTask);
 }
 
-void Spotify::show()
-{
+void Spotify::refreshInfo()
+{   
     while (true)
-    {
-      // Market can be excluded if you want e.g. spotify.getCurrentlyPlaying()
-      int status = spotify->getCurrentlyPlaying([&](CurrentlyPlaying currentlyPlaying)
-                                                { 
-                        isPlaying = currentlyPlaying.isPlaying;
+    {   
+        int status;
+        switch (patternStatus)
+        {
+            case playback:
+                status = spotify->getCurrentlyPlaying([&](CurrentlyPlaying currPlaying)
+                                                      { 
+                        ESP_LOGI(__func__,"Got Currently Playing");
+                        currentlyPlaying = currPlaying;
                         lastUpdate = millis();
-                        lastProgress = currentlyPlaying.progressMs;
-                        duration = currentlyPlaying.durationMs;
+                        ESP_LOGI(__func__,"Updated vals");
                         if (currentlyPlaying.isPlaying)
                         {
                             String currentTrack = String(currentlyPlaying.trackUri);
+                            this->lastPlaying = millis();
                             if (currentTrack.compareTo(previousTrack) != 0)
                             {
                                 String currentAlbum = String(currentlyPlaying.albumUri);
-                                ESP_LOGI(__func__,"New Track - Updating Text (%s -> %s)\n", previousTrack.c_str(), currentlyPlaying.trackUri);
-                                displayInfo(currentlyPlaying);
+                                ESP_LOGI(__func__, "New Track - Updating Text (%s -> %s)\n", this->previousTrack.c_str(), currentlyPlaying.trackUri);
+                                displayInfo();
                                 if (currentAlbum.compareTo(previousAlbum) != 0)
                                 {
-                                    ESP_LOGI(__func__,"New Album - Updating Art (%s -> %s)\n", previousAlbum.c_str(), currentlyPlaying.albumUri);
-                                    displayImage(currentlyPlaying);
+                                    ESP_LOGI(__func__, "New Album - Updating Art (%s -> %s)\n", this->previousAlbum.c_str(), currentlyPlaying.albumUri);
+                                    displayImage();
                                 }
                             }
-                            previousTrack = currentlyPlaying.trackUri;
-                            previousAlbum = currentlyPlaying.albumUri;
+                        }
+                        else if (millis() - lastPlaying > BLANK_AFTER_PAUSE_MS)
+                        {
+                            changeStatus(noPlayback);
+                        }                                    
+                        previousTrack = currentlyPlaying.trackUri;
+                        previousAlbum = currentlyPlaying.albumUri; });
+                if (status > 300)
+                {
+                    ESP_LOGI(__func__, "Error: %d", status);
+                }
+                status = spotify->getPlayerDetails([&](PlayerDetails current)
+                                                   { 
+                                                playerDetails = current;
+                                                if (patternStatus == playback)
+                                                {
+                                                    this->displayPlayback();
+                                                } });
+                if (status > 300)
+                {
+                    ESP_LOGI(__func__, "Error: %d", status);
+                }
+                vTaskDelay(300 / portTICK_PERIOD_MS);
+                break;
+            case noPlayback:
+                status = spotify->getCurrentlyPlaying([&](CurrentlyPlaying currPlaying)
+                                                          {      
+                        if (currPlaying.isPlaying)
+                        {
+                            changeStatus(playback);
+                            currentlyPlaying = currPlaying;
+                            lastUpdate = millis();
+                            status = spotify->getPlayerDetails([&](PlayerDetails current)
+                                                               { 
+                                                playerDetails = current;
+                                                this->displayPlayback(); });
+                            if (status > 300)
+                            {
+                                ESP_LOGI(__func__, "Error: %d", status);
+                            }
+                            displayImage();
+                            displayInfo();
                         } });
-      if (status > 300)
-      {
-          ESP_LOGI(__func__, "Error: %d", status);
-      }
-      status = spotify->getPlayerDetails([&](PlayerDetails current)
-                                         { this->displayPlayback(current); });
-      if (status > 300)
-      {
-          ESP_LOGI(__func__, "Error: %d", status);
-      }
-      vTaskDelay(300 / portTICK_PERIOD_MS);
+                if (status > 300)
+                {
+                    ESP_LOGI(__func__, "Error: %d", status);
+                }
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+                break;
+            default:
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+                break;
+        }
     }
 }
 
-bool Spotify::displayOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_t *bitmap)
+bool Spotify::displayImageOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_t *bitmap)
 {
     // Stop further decoding as image is running off bottom of screen
     if (y >= panel0->height())
@@ -221,7 +323,7 @@ bool Spotify::displayOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_
     return 1;
 }
 
-int Spotify::displayImage(CurrentlyPlaying currentlyPlaying)
+int Spotify::displayImage()
 {
     SpotifyImage smallestImage = currentlyPlaying.albumImages[currentlyPlaying.numImages - 1];
     String newAlbum = String(smallestImage.url);
@@ -246,7 +348,7 @@ int Spotify::displayImage(CurrentlyPlaying currentlyPlaying)
     }
 }
 
-void Spotify::displayInfo(CurrentlyPlaying currentlyPlaying)
+void Spotify::displayInfo()
 {
     panel1->fillRect(0, 0, 64, 38, 0x0000);
     panel1->setTextColor(0xFFFF);
@@ -277,29 +379,32 @@ void Spotify::displayProgress()
 {
     while (true)
     {
-        long progress = lastProgress + (millis() - lastUpdate);
-        if (!isPlaying)
+        if (currentlyPlaying.isPlaying || (millis() - lastUpdate < BLANK_AFTER_PAUSE_MS))
         {
-            progress = lastProgress;
-        }
-        if (progress > duration)
-        {
-            progress = duration;
-        }
-        if (duration > 0)
-        {
-            panel1->drawLine(0, 39, 63, 39, panel1->color565(50, 50, 50));
-            int barLength = (progress * 63) / (duration);
-            long rem = (progress * 63) % (duration);
-            int bright = (rem * 205 / duration) + 50;
-            panel1->drawLine(0, 39, barLength, 39, panel1->color565(255, 255, 255));
-            panel1->drawPixelRGB888(barLength + 1, 39, bright, bright, bright);
+            long progress = currentlyPlaying.progressMs + (millis() - lastUpdate);
+            if (!currentlyPlaying.isPlaying)
+            {
+                progress = currentlyPlaying.progressMs;
+            }
+            if (progress > currentlyPlaying.durationMs)
+            {
+                progress = currentlyPlaying.durationMs;
+            }
+            if (currentlyPlaying.durationMs > 0)
+            {
+                panel1->drawLine(0, 39, 63, 39, panel1->color565(50, 50, 50));
+                int barLength = (progress * 63) / (currentlyPlaying.durationMs);
+                long rem = (progress * 63) % (currentlyPlaying.durationMs);
+                int bright = (rem * 205 / currentlyPlaying.durationMs) + 50;
+                panel1->drawLine(0, 39, barLength, 39, panel1->color565(255, 255, 255));
+                panel1->drawPixelRGB888(barLength + 1, 39, bright, bright, bright);
+            }
         }
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 
-void Spotify::displayPlayback(PlayerDetails playerDetails)
+void Spotify::displayPlayback()
 {
     if(playerDetails.isPlaying){
         panel1->drawSprite16(spotify_pause, 24, 44, 16, 16, 100,100,100,true);
--- lib/patterns/spotify/spotify.h
+++ lib/patterns/spotify/spotify.h
@@ -20,9 +20,23 @@
 #include "secrets.h"
 #endif
 
+#define BLANK_AFTER_PAUSE_MS 300000
+#define SPOTIFY_REQUEST_INTERVAL_MS 1000
+#define PROGRESS_REFRESH_MS 100
+
+//#define SPOTIFY_RESET_OAUTH
+//#define SPOTIFY_RESET_TOKEN
+
 // get ESP-IDF Certificate Bundle
 extern const uint8_t rootca_crt_bundle_start[] asm("_binary_x509_crt_bundle_start");
 
+enum PatternStatus
+{
+  oauth,
+  refreshToken,
+  noPlayback,
+  playback
+};
 
 class Spotify : public Pattern
 {
@@ -34,12 +48,16 @@ public:
   ~Spotify();
 
 private:
-  void show();
-  bool displayOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_t *bitmap);
-  int displayImage(CurrentlyPlaying currentlyPlaying);
-  void displayInfo(CurrentlyPlaying currentlyPlaying);
+  void refreshInfo();
+  bool displayImageOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint8_t *bitmap);
+  int displayImage();
+  void displayInfo();
   void displayProgress();
-  void displayPlayback(PlayerDetails playerDetails);
+  void displayPlayback();
+  void startOauthWebServer();
+  void stopOauthWebServer();
+  int setupCredentials();
+  void changeStatus(PatternStatus status);
 
   SinglePanel *panel0;
   SinglePanel *panel1;
@@ -52,10 +70,13 @@ private:
 
   String previousTrack;
   String previousAlbum;
+  long lastPlaying;
   long lastUpdate;
-  long lastProgress;
-  long duration;
-  bool isPlaying;
+
+  CurrentlyPlaying currentlyPlaying;
+  PlayerDetails playerDetails;
+  PatternStatus patternStatus;
+  std::vector<AsyncCallbackWebHandler *> handlers;
 
   char spotifyID[33];
   char spotifySecret[33];
